33cd043cf26e3a76819a0d86709ccdb0dc2dc5c3
Thiago Nobrega <thiagons@live.com>
2022-04-17 12:12:00 -0300

    refactor: refactoring header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,35 @@
-import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import myContext from "../context/AppContext";
+import React from "react";
 import '../styles/Header.css';
+import MenuBar from "../components/MenuBar";
+import react_icon from '../images/react_icon.svg'
+import js_icon from '../images/js_icon.svg'
+import html_icon from '../images/html_icon.svg'
+import css_icon from '../images/css_icon.svg'
 
 function Header() {
-  const navigate = useNavigate();
-  const { selectedPage, setSelectedPage } = useContext(myContext)
-  const selectPage = ({target: {name}}) => {
-    setSelectedPage(name)
-    navigate(`/${name}`);
-  }
   return ( 
     <header className="header-container">
-      <nav>
-        <button
-          type="button"
-          name=""
-          className={selectedPage === '' ? 'nav-bar active' : 'nav-bar'}
-          onClick={selectPage}
-        >
-          home.jsx
-        </button>
-        <button
-          type="button"
-          name="about"
-          className={selectedPage === 'about' ? 'nav-bar active' : 'nav-bar'}
-          onClick={selectPage}
-        >
-          about.html
-        </button>
-        <button
-          type="button"
-          name="projects"
-          className={selectedPage === 'projects' ? 'nav-bar active' : 'nav-bar'}
-          onClick={selectPage}
-        >
-          projects.js
-        </button>
-        <button
-          type="button"
-          name="contact"
-          className={selectedPage === 'contact' ? 'nav-bar active' : 'nav-bar'}
-          onClick={selectPage}
-        >
-          contact.css
-        </button>
+      <nav className="nav-container">
+        <MenuBar 
+          icon={ react_icon }
+          name='home.jsx'
+          path="/"
+        /> 
+        <MenuBar 
+          icon={ html_icon }
+          name='about.html'
+          path="/about"
+        />
+        <MenuBar 
+          icon={ js_icon }
+          name='projects.js'
+          path="/projects"
+        />
+        <MenuBar 
+          icon={ css_icon }
+          name='contact.css'
+          path="/contact"
+        />  
       </nav>
     </header>
   );
