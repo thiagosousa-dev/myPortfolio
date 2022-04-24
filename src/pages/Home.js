214525fe32d9ca67ab4c45746b6f00623b21d376
Thiago Nobrega <thiagons@live.com>
2022-04-24 12:35:20 -0300

    feat: adicionando imagem no Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,11 @@ import Header from "../components/Header";
 import TitleBar from "../components/TitleBar";
 import Footer from "../components/Footer";
 import SideBar from "../components/SideBar";
+import '../styles/Home.css';
+import image from '../images/image.png';
+import { FiDownload } from 'react-icons/fi';
+import MenuMobile from "../components/MenuMobile";
+import Typical from 'react-typical';
 
 function Home() {
   return ( 
@@ -11,8 +16,22 @@ function Home() {
       <section className="flex-container">
         <section className="main-container">
           <Header />
-          <main className="main-wrapper">
-            <h1>Home</h1>
+          <MenuMobile />
+          <main className="main-wrapper home">
+            <div className="home-content">
+              <p><span className="span">{'<'}</span>Hi there 👋 I'm</p>
+              <h1>Thiago Nóbrega <span className="span">{'/>'}</span> </h1>
+              <Typical 
+                steps={['Front-End Developer', 1000, 'Full Stack Web Development Student', 1000]}
+                loop={Infinity}
+                wrapper="h2"
+              />
+              <button type="button" className="btn-download">
+                Download CV
+                <FiDownload />
+              </button>
+            </div>
+            <img src={image} alt="imag" className="image"/>
           </main>
         </section>
         <SideBar />
