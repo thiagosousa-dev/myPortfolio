e48d23106658ae652c2dcbed37939712ccd77d41
Thiago Nobrega <thiagons@live.com>
2022-04-24 12:38:57 -0300

    feta: adicionando menu mobile a tela projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import TitleBar from "../components/TitleBar";
 import Footer from "../components/Footer";
 import SideBar from "../components/SideBar";
+import MenuMobile from "../components/MenuMobile";
 
 function Projects() {
   return ( 
@@ -11,6 +12,7 @@ function Projects() {
     <section className="flex-container">
       <section className="main-container">
         <Header />
+        <MenuMobile />
         <main className="main-wrapper">
           <h1>Projects</h1>
         </main>
